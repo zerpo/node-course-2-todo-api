f6633eaafae3f39ec4dfedb070cf9521f632e87f
Sebastian Feurer <sebastian.feurer@pb.com>
2017-06-17 10:19:40 +0200

    init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,44 @@
+// const MongoClient = require ('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb')
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=>{
+  if(err)  {
+    return console.log('Unable to connect to database server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos')
+  // .find({_id: new ObjectID('5944dce26f9ee20c996225f8')})
+  // .toArray()
+  // .then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) =>{
+  //   console.log('Unable to fetch todos', err);
+  // })
+
+
+  // db.collection('Todos')
+  // .find()
+  // .count()
+  // .then((count) => {
+  //   console.log(`Todos count ${count}`);
+  //
+  // }, (err) =>{
+  //   console.log('Unable to fetch todos', err);
+  // })
+
+  db.collection('Users')
+  .find({name: 'Sebastian'})
+  .toArray()
+  .then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) =>{
+    console.log('Unable to fetch todos', err);
+  })
+
+
+
+  // db.close();
+});
