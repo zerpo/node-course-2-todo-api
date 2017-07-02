10b6144f9f4459e2ebadd741877bd0d251bb5424
Sebastian Feurer <sebastian.feurer@pb.com>
2017-07-02 19:29:54 +0200

    Added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
+require ('./config/config')
 const _ = require('lodash')
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
@@ -8,7 +9,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 app.use(bodyParser.json());
